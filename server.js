e7626301bb8b460a9568db9956af499bf29c2e64
Patrick Conde <patrick.conde.costas@gmail.com>
2019-01-25 00:41:34 -0500

    refactoring

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const keys = require('./config/keys');
 
 const app = express();
 
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Serve static files from React into an auto created build folder
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 
 // mLab
@@ -24,13 +27,18 @@ connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
 
+// Post
+const postRoutes = require('./controllers/postController');
+app.use('/api/posts', postRoutes);
+
+
+// If any request doesn't match, we send it back to React
+// build file created after the push to heroku is made
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+})
 
 const PORT = process.env.PORT || 3000; // for production deployment use standard host port, for dev we use 3000
 app.listen(PORT, () => {
   console.log("Server is running on Port: ", PORT);
 })
-
-
-// Post
-const postRoutes = require('./controllers/postController');
-app.use('/api/posts', postRoutes);
