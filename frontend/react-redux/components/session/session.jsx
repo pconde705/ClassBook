52ce24b5ce6e889f09d404d472b6aa4ce4caa689
Patrick Conde <patrick.conde.costas@gmail.com>
2019-02-07 14:13:31 -0500

    added fonts

--- frontend/react-redux/components/session/session.jsx
+++ frontend/react-redux/components/session/session.jsx
@@ -15,30 +15,41 @@ class Session extends React.Component {
   render() {
     return (
       <div>
-        <header className="sign-up-header">
-          <div>
-            <p className="sign-up-logo">classbook</p>
-          </div>
-          <div className="sign-up-forms">
+        <header className="login-navbar">
+          <div className="login-header">
             <div>
-              <p>Email</p>
-              <input></input>
+              <p className="login-logo">classbook</p>
             </div>
-            <div>
-              <p>Password</p>
-              <input></input>
-            </div>
-            <button>Log In</button>
+            <table>
+              <tbody>
+                <tr>
+                  <td><p className="login-p">Email</p></td>
+                  <td><p className="login-p">Password</p></td>
+                </tr>
+                <tr>
+                  <td className="login-td"><input className="login-input"></input></td>
+                  <td className="login-td"><input className="login-input"></input></td>
+                  <td><button className="login-button">Log In</button></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </header>
-        <main>
+        <main className="signup-main">
           <div>
-            <p>Connect with you friends</p>
-          </div>
-          <div>
-            <p>Sign Up</p>
+            <div>
+              <p>Connect with you friends</p>
+            </div>
+            <div>
+              <p>Sign Up</p>
+            </div>
           </div>
         </main>
+        <footer>
+          <div>
+
+          </div>
+        </footer>
       </div>
     );
   }
